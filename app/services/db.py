c8d77142041cedb76daece6679ef7fa8821d0442
autobodyinsight <jorge@autobodyinsight.com>
2026-01-27 00:33:09 +0000

    Add SSL mode requirement to database connection and new tech assignment endpoints

--- app/services/db.py
+++ app/services/db.py
@@ -4,4 +4,4 @@ import psycopg2.extras
 
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://management_app_yj3h_user:VioUE4I0r3VgaBiNt920IbTXfbRT9dfc@dpg-d5qf43juibrs73c4q5o0-a.oregon-postgres.render.com/management_app_yj3h")
 
-conn = psycopg2.connect(DATABASE_URL, cursor_factory=psycopg2.extras.RealDictCursor)
+conn = psycopg2.connect(DATABASE_URL, cursor_factory=psycopg2.extras.RealDictCursor, sslmode='require')
