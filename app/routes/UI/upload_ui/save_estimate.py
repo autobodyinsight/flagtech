6ca4186648a5b95d5bf2f2c6a85b4caec37e59d7
autobodyinsight <jorge@autobodyinsight.com>
2026-02-08 19:29:21 +0000

    updated files

--- app/routes/UI/upload_ui/save_estimate.py
+++ app/routes/UI/upload_ui/save_estimate.py
@@ -218,7 +218,7 @@ var preloadedEstimateTotals = {json.dumps({
     "insurance_pay": insurance_pay,
 })};
 
-window.currentEstimateTotals = Object.assign({}, preloadedEstimateTotals);
+window.currentEstimateTotals = Object.assign({{}}, preloadedEstimateTotals);
 
 // Vehicle info parsed from vehicle_info_line
 var vehicleYear = null;
